c495efb9ec04ad502a19e7970c9a25801b45e6ed
Daniel Fong <daniel.fong.mv@gmail.com>
2020-01-30 10:43:17 -0700

    implemented delete button

--- src/List.js
+++ src/List.js
@@ -11,9 +11,11 @@ export default function List(props) {
       <div className='List-cards'>
         {props.cards.map((card) =>
           <Card
+            id={card.id}
             key={card.id}
             title={card.title}
             content={card.content}
+            onDeleteCard={props.onDeleteClick}
           />
         )}
         <button
